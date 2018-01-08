b1dbc87bcfd07d1a67ea03e7f4e66bbb0ab7391c
Daniel Hochleitner <dhochleitner@posteo.de>
2018-01-08 13:12:24 +0100

    - unset SQLPATH env var if present --> ensure SQLcl runs without e.g login.sql file - added npm update mechansim / notifier if newer version of package is on npm

--- bin/apde.js
+++ bin/apde.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-//const update = require('update-notifier');
-//const pkg = require('../package.json');
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
 const apde = require('../index.js');
 
 // Workaround for MacOS which doesn't allow to sigint
@@ -13,23 +13,28 @@ process.on('SIGINT', () => {
 // get command line arguments
 const userArgs = process.argv.slice(2);
 
-// Check for updates once a day
-/*const notifier = update({
-	packageName: pkg.name,
-	packageVersion: pkg.version
+// Check for updates on npm
+const notifier = updateNotifier({
+  pkg
 });
 
 if (notifier.update) {
-	notifier.notify({
-		defer: false,
-		message: chalk.bold('APDE') + ' update available ' +
-			chalk.dim(notifier.update.current) +
-			chalk.reset(' → ') +
-			chalk.green(notifier.update.latest) +
-			' \nRun:\n' + chalk.cyan.bold('npm install -g apex-plugin-dev-enhancer')
-	});
-}*/
+  notifier.notify({
+    defer: false,
+    message: chalk.bold('APDE') + ' update available ' +
+      chalk.dim(notifier.update.current) +
+      chalk.reset(' → ') +
+      chalk.green(notifier.update.latest) +
+      ' \nRun:\n' + chalk.cyan.bold('npm install -g apex-plugin-dev-enhancer')
+  });
+}
 
+// delete Oracle SQLPATH environment variable (e.g login.sql) if present --> ensure SQLcl works as expected
+if (typeof process.env.SQLPATH !== 'undefined') {
+  delete process.env.SQLPATH;
+}
+
+// call APDE commands
 if (typeof userArgs[0] === 'undefined') {
   apde.help();
 } else if (typeof apde[userArgs[0]] === 'undefined') {
